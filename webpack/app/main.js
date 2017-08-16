19d96db3e176f1e7dacf3f01ca6d8213db8f1a84
lizc <291769886@qq.com>
2017-08-16 10:44:33 +0800

    add webpack demo

---
+++ webpack/app/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lzc on 2017/8/15.
+ */
+// const greeter = require('./Greeter.js');
+// document.querySelector("#root").appendChild(greeter());
+
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import './main.css';
+
+render(<Greeter />, document.getElementById('root'));
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,16 @@
 /**
  * Created by lzc on 2017/8/15.
  */
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: __dirname + "/app/main.js",
     output: {
-        path: __dirname + '/product',
-        filename: 'bundle-[hash].js'
+        path: __dirname + '/public',
+        filename: 'bundle.js'
     },
     devServer: {
-        contentBase: "./product",//本地服务器所加载的页面所在的目录
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         inline: true//实时刷新
     },
@@ -36,27 +34,23 @@ module.exports = {
         //         ]
         //     }
         // ]
-        loaders: [
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader']
-            }
+            loaders: [
+                {
+                    test: /\.json$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.css$/,
+                    loaders: ['style-loader', 'css-loader']
+                }
         ]
 
     },
     plugins: [
-        new webpack.BannerPlugin('版权所有，翻版必究'),
-        new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
-        })
     ],
 }
