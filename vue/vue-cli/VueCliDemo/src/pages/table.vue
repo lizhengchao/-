ff45e584d058a79fe056580304e8086ac7178d20
lizc <291769886@qq.com>
2017-08-24 15:04:29 +0800

    添加 vue 动态组件demo， 添加table demo， update vue-router demo

---
+++ vue/vue-cli/VueCliDemo/src/pages/table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="demo">
+    <form id="search">
+      Search <input name="query" v-model="searchQuery">
+    </form>
+    <demoGrid
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery">
+    </demoGrid>
+  </div>
+</template>
+<script>
+  import demoGrid from '../components/demo-grid.vue'
+  export default {
+    name: 'table',
+    components: {
+      demoGrid: demoGrid
+    },
+    data () {
+      return {
+        searchQuery: '',
+        gridColumns: ['name', 'power'],
+        gridData: [
+          { name: 'Chuck Norris', power: Infinity },
+          { name: 'Bruce Lee', power: 9000 },
+          { name: 'Jackie Chan', power: 7000 },
+          { name: 'Jet Li', power: 8000 }
+        ]
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
