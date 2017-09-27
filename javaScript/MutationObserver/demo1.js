2f09f54c48224261e946444ade1f466c9d2db10f
lizc <291769886@qq.com>
2017-09-27 11:03:00 +0800

    加入普通的事件监听dom做比较

--- javaScript/MutationObserver/demo1.js
+++ javaScript/MutationObserver/demo1.js
@@ -1,7 +1,7 @@
 /**
  * Created by lzc on 2017/9/27.
  */
-(() => {
+(() => { //使用MutaionObserver监听document.body
     var MutationObserver = window.MutationObserver || window.WebkitMutaionObserver || window.MozMuationObserver;
     if(!MutationObserver) {
         alert("当前浏览器不支持MutationObserver");
@@ -10,7 +10,7 @@
     var moCallback = (records) => {
         console.info("mo callback");
         records.map((record)=> {
-                debugger
+                console.info('mo record callback');
         })
     },
         option = {
@@ -22,7 +22,14 @@
     var mo = new MutationObserver (moCallback);
 
     mo.observe(document.body, option);
+})();
 
+(()=> { //使用事件监听document.body,会立即执行
+    document.body.addEventListener('DOMSubtreeModified', (e)=> {
+        console.info('DOMSubtreeModified')})
+})();
+
+(() => { //点击事件
     var btn = document.getElementById('tapbtn'),
         addAtr = document.getElementById('addAtr'),
         appDiv = document.getElementById('app');
@@ -31,9 +38,10 @@
             var p = document.createElement('p');
             p.innerHTML = i;
             appDiv.appendChild(p);
+            console.info('add node');
         }
     })
     addAtr.addEventListener('click', ()=> {
-        appDiv.setAttribute('class', "red");
+        appDiv.setAttribute('class', 'red');
     })
 })()
