7ce17482df60f08db025b9879787020f95e1c6ac
lizc <291769886@qq.com>
2017-10-11 16:49:57 +0800

    完成基本的滑动交换逻辑

--- React/xiaoxiaole/src/block/view/Block.js
+++ React/xiaoxiaole/src/block/view/Block.js
@@ -2,7 +2,7 @@
  * Created by lzc on 2017/10/11.
  */
 import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import './Block.css'
 
 class Block extends Component {
@@ -10,14 +10,18 @@ class Block extends Component {
         super(props, context);
 
         this.getColorByStatus = this.getColorByStatus.bind(this);
-        
+        this.touchStart = this.touchStart.bind(this);
+        this.touchMove = this.touchMove.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
+
+        this.touchStartX = 0;
+        this.touchStartY = 0;
+        this.touchEndX = 0;
+        this.touchEndY = 0;
+
         this.state = {
-            color: this.getColorByStatus(props.status)
+            color: this.getColorByStatus(props.status),
         }
-    }
-    
-    getColorByStatus (status) {
-        return status;
     }
 
     componentWillReceiveProps (nextProps) {
@@ -26,9 +30,56 @@ class Block extends Component {
 
     render () {
         return (
-            <div className="block" style={{'backgroundColor': this.state.color}}></div>
+            <div className="block" style={{'backgroundColor': this.state.color}}
+                 onTouchStart={this.touchStart} onTouchMove={this.touchMove} onTouchEnd={this.touchEnd}></div>
         )
     }
+
+    getColorByStatus (status) {
+        return status;
+    }
+
+    touchStart (e) {
+        this.touchStartX = e.touches[0].clientX;
+        this.touchStartY = e.touches[0].clientY;
+    }
+
+    touchMove (e) {
+        this.touchEndX = e.touches[0].clientX;
+        this.touchEndY = e.touches[0].clientY;
+    }
+
+    touchEnd (e) {
+        //返回滑动的方向和所在位置
+        let touchMoveX = this.touchEndX - this.touchStartX,
+            touchMoveY = this.touchEndY - this.touchStartY,
+            moveType = '';
+        if (Math.abs(touchMoveX) > Math.abs(touchMoveY)){
+            if (touchMoveX > 0) {
+                moveType = 'right';    
+            } else {
+                moveType = 'left';  
+            }
+        } else if (Math.abs(touchMoveX) < Math.abs(touchMoveY)) {
+            if (touchMoveY > 0) {
+                moveType = 'down';
+            } else {
+                moveType = 'up';
+            }
+        }
+        this.props.move({
+            row: this.props.row, 
+            line: this.props.line, 
+            moveType: moveType
+        });
+    }
+}
+
+Block.propTypes = {
+    status: PropTypes.string,
+    line: PropTypes.number,
+    row: PropTypes.number,
+    move: PropTypes.fun
 }
 
 export default Block;
--- React/xiaoxiaole/src/block/view/Container.js
+++ React/xiaoxiaole/src/block/view/Container.js
@@ -3,6 +3,7 @@
  */
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {updateBlockStatus} from '../actions'
 import Block from './Block'
 import './Container.css'
 
@@ -10,22 +11,63 @@ class Container extends Component {
     constructor (props, context) {
         super(props, context);
 
+        this.getOwnState = this.getOwnState.bind(this);
+        this.blockMove = this.blockMove.bind(this);
+
         this.state = {
-            blockStatus: this.context.store.getState().blockStatus
+            blockStatus: this.getOwnState()
         }
     }
 
+    componentDidMount () {
+        this.context.store.subscribe(() => {
+            this.setState({blockStatus: this.getOwnState()});
+        })
+    }
+
+    getOwnState () {
+        return this.context.store.getState().blockStatus;
+    }
+    
+    blockMove ({row, line, moveType}) {
+        let swapBlock = (rowA, lineA, rowB, lineB) => {
+            var colorA = this.getOwnState()[rowA][lineA],
+                colorB = this.getOwnState()[rowB][lineB];
+
+            this.context.store.dispatch(updateBlockStatus(rowA, lineA, colorB));
+            this.context.store.dispatch(updateBlockStatus(rowB, lineB, colorA));
+        }
+
+        switch (moveType) {
+            case 'left':
+                swapBlock(row, line, row, line-1);
+                break;
+            case 'right':
+                swapBlock(row, line, row, line+1);
+                break;
+            case  'up':
+                swapBlock(row, line, row-1, line);
+                break;
+            case 'down':
+                swapBlock(row, line, row+1, line);
+                break;
+        }
+    }
+
+
     render () {
-        const blockStatus = Object.assign({}, this.state.blockStatus);
+        console.info('render container');
+        const blockStatus = this.state.blockStatus;
         var blocks = [],
             rowBlock;
 
         for(let row in blockStatus) {
             rowBlock = [];
             for(let line in blockStatus[row]){
-                rowBlock.push(<Block key={row*100+line}  status={blockStatus[row][line]}/>)
+                rowBlock.push(<Block key={line}  status={blockStatus[row][line]} 
+                                     row={parseInt(row)} line={parseInt(line)} move={this.blockMove}/>)
             }
-            rowBlock = (<div class="row">{rowBlock}</div>);
+            rowBlock = (<div key={row} className="row">{rowBlock}</div>);
             blocks.push(rowBlock);
         }
 
