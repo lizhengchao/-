73c2496a3851c6363a579fb48cd6b8ca1d02b456
lizc <291769886@qq.com>
2017-10-16 17:28:36 +0800

    将Block的动画独立出来

--- React/xiaoxiaole/src/block/view/Block.js
+++ React/xiaoxiaole/src/block/view/Block.js
@@ -19,97 +19,124 @@ class Block extends Component {
         this.touchEndX = 0;
         this.touchEndY = 0;
 
-        this.transform = 'translate(0px, 0px)';
-        this.transition= 'transform 0.5s';
-
         this.animationQueue = []; //当前块的动画队列
+        this.isStop = false; //记录动画阻塞状态
+
+        this.transform = '';
+        this.transition = '';
+        this.opacity = '';
 
         this.state = {
-            color: this.getColorByStatus(props.status),
-            animation: ''
+            color: this.getColorByStatus(props.status)/*,
+            transform: '',
+            transition: '',
+            opacity: ''*/
         }
     }
 
     componentWillReceiveProps (nextProps) {
-        if(this.state.animation != (typeof nextProps.animation == 'undefined' ? '' : nextProps.animation)){
-            this.setState({animation: nextProps.animation});
-            setTimeout(() => { //等待动画完成后移动回原本的位置
-                this.setState({animation: ''});
-            }, 500);
-
-        }
-        if(this.getColorByStatus(nextProps.status) !== this.state.color) {
-            setTimeout(() => { //等待动画完成后再更新颜色
-                this.setState({color: this.getColorByStatus(nextProps.status,)});
-            }, 500);
-        }
-
-        // let pushAnimation = (newAnimation) => {
-        //     switch (newAnimation.type) {
-        //         case 'setColor':
-        //             break;
-        //         case 'setTimeout':
-        //             break;
-        //         case 'move':
-        //             break;
-        //         case 'disappear':
-        //             break;
-        //         default:
-        //     }
-        // };
-
         if(!nextProps.newAnimation){
             return;
         } else if(nextProps.newAnimation.constructor === Array){
             //如果是数组则说明是多个动画
             nextProps.newAnimation.forEach((newAnimation) => {
                 this.animationQueue.push(newAnimation);
-                this.setState({});
             })
         } else {
             this.animationQueue.push(nextProps.newAnimation);
-            this.setState({});
+        }
+
+        let
+            dealColor = (animation) => {
+                this.setState({
+                    color: animation.color
+                });
+                dealAnimations();
+            },
+            dealMove = (animation) => {
+                switch (animation.moveType){
+                    case 'left':
+                        this.transition = 'transform 0.5s';
+                        this.transform = 'translate(-35px, 0px)';
+                        break;
+                    case 'right':
+                        this.transition = 'transform 0.5s';
+                        this.transform = 'translate(35px, 0px)';
+                        break;
+                    case 'up':
+                        this.transition = 'transform 0.5s';
+                        this.transform = 'translate(0px, -35px)';
+                        break;
+                    case 'down':
+                        this.transition = 'transform 0.5s';
+                        this.transform = 'translate(0px, 35px)';
+                        break;
+                    default:
+                        this.transition = 'transform 0s';
+                        this.transform = '';
+                }
+                this.setState({});
+                setTimeout(()=> {
+                    this.isStop = true;
+                }, 0);
+                setTimeout(() => {
+                    this.isStop = false;
+                    this.transition = 'transform 0s';
+                    this.transform = '';
+                    dealAnimations();
+                }, 500);
+            },
+            dealDisappear = (animation) => {
+                this.opacity = '0';
+                this.transition = 'opacity 0.5s';
+                this.setState({});
+                setTimeout(()=>{
+                   this.isStop = true;
+                },0);
+                setTimeout(() => {
+                    this.isStop = false;
+                    this.transition = 'all 0s';
+                    this.opacity = '1';
+                    dealAnimations();
+                }, 500);
+            },
+            dealAnimations = () => {
+                let curAnimation = this.animationQueue.shift();
+                if(curAnimation) {
+                    switch (curAnimation.type) {
+                        case 'setColor': //更新颜色
+                            dealColor(curAnimation);
+                            break;
+                        case 'move':
+                            dealMove(curAnimation);
+                            break;
+                        case 'disappear':
+                            dealDisappear(curAnimation);
+                            break;
+                        default:
+                    }
+                }
+            };
+        if(!this.isStop){
+            dealAnimations();
         }
     }
 
     shouldComponentUpdate (nextProps, nextState) {
-        if(this.getColorByStatus(nextProps.status) === this.state.color &&
-            nextState.animation === this.state.animation){
-            return false;
-        } else {
+        if(nextProps.newAnimation &&!this.isStop){
             return true;
+        } else {
+            return false;
         }
     }
 
     componentWillUpdate (nextProps, nextState) {
-
-        switch (nextState.animation){
-            case 'left':
-                this.transition = 'transform 0.5s';
-                this.transform = 'translate(-35px, 0px)';
-                break;
-            case 'right':
-                this.transition = 'transform 0.5s';
-                this.transform = 'translate(35px, 0px)';
-                break;
-            case 'up':
-                this.transition = 'transform 0.5s';
-                this.transform = 'translate(0px, -35px)';
-                break;
-            case 'down':
-                this.transition = 'transform 0.5s';
-                this.transform = 'translate(0px, 35px)';
-                break;
-            default:
-                this.transition = 'transform 0s';
-                this.transform = '';
-        }
     }
 
     render () {
         console.info('render block');
         return (
-            <div className="block" style={{'backgroundColor': this.state.color, transform: this.transform, transition: this.transition}}
+            <div className="block" style={{'backgroundColor': this.state.color, transform: this.transform, transition: this.transition, opacity: this.opacity}}
                  onTouchStart={this.touchStart} onTouchMove={this.touchMove} onTouchEnd={this.touchEnd}></div>
         )
     }
@@ -159,8 +186,8 @@ Block.propTypes = {
     line: PropTypes.number,
     row: PropTypes.number,
     touch: PropTypes.func,
-    animation: PropTypes.string, //如果有该参数则说明需要做动画
-    newAnimation: PropTypes.object  //接下来要完成的新动画
+    // animation: PropTypes.string, //如果有该参数则说明需要做动画
+    // newAnimation: PropTypes.object  //接下来要完成的新动画
 }
 
 export default Block;
